583eeebc9a0495810a2661086d9d47c8faf9a25e
KH-Kim <kh9768@gmail.com>
2021-03-23 14:19:03 +0900

    2021-03-23 김광희 첫번째 성공

--- python/SocketTest_02/Socket_Client_02.py
+++ python/SocketTest_02/Socket_Client_02.py
@@ -2,26 +2,33 @@ from socket import *
 from os.path import exists
 import os
 import sys
-import picamera
+from picamera import PiCamera
 import time
 
 # 함수 - 사진 캡처
+"""
 def cap_img(filename):
     camera.capture(filename)
+    
+"""
 
 # 변수 선언
 data_transferred = 0
 
 # 서버와 연결
 clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect(('127.0.0.1', 9000))
+clientSocket.connect(('127.0.0.1', 9002))
 
 print("연결에 성공했습니다.")
 
-# 카메라 생성
+'''# 카메라 생성
 with picamera.PiCamera() as camera:
     # 카메라 해상도 설정
-    camera.resolution = (1024,768)
+    camera.resolution = (1024, 768)
+'''
+
+camera = PiCamera()
+camera.resolution = (1024, 768)
 
 select_mode = 0
 
@@ -48,6 +55,7 @@ while True:
         filename = input("파일명을 입력하세요(확장자 제외) : ")
         filename = filename + '.jpg'
 
+        
         # 파일명 전송
         try:
             clientSocket.sendall(filename.encode('utf-8'))
@@ -55,21 +63,25 @@ while True:
             print(ex)
             print("파일명 전송과정 오류")
             continue
+        
 
         #사진 찍음
-        cap_img(filename)
-
-        time.sleep(0.5)
+        #cap_img(filename)
+        camera.capture(filename)
+        time.sleep(6)
 
         if not exists(filename):
             print("no file, 사진 파일 없음")
             sys.exit()
-        
+
+        #사진찍음
+        #os.system('raspistill -o ' + filename)
+
         with open(filename, 'rb') as f:
             try:
                 data = f.read(1024)
                 while data:
-                    data_transferred += connectionSocket.send(data)
+                    data_transferred += clientSocket.send(data)
                     data = f.read(1024)
 
             except Exception as ex:
--- python/SocketTest_02/Socket_Server_02.py
+++ python/SocketTest_02/Socket_Server_02.py
@@ -6,33 +6,39 @@ import sys
 # 참고사이트 : https://foxtrotin.tistory.com/278
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('', 9000))
+serverSocket.bind(('', 9002))
 serverSocket.listen(1)
 
 connectionSocket, addr = serverSocket.accept()
 
 print(str(addr), '에서 접속했습니다.')
 
+
 while True:
+       
+    data = connectionSocket.recv(1024)
+    
     
     try:
-        filename = connectionSocket.recv(1024)
+        filename = data
+        print("받는파일명 : ", filename)
     except Exception as ex:
         print(ex)
         print("파일명 받는 과정 오류")
         continue
-
+    
+    
     nowdir = os.getcwd()
     
     # 데이터 받음
-    data = serverSocket.recv(1024)
+    data = connectionSocket.recv(1024)
     data_transferred = 0
-    with open(nowdir+"\\"+filename, 'wb') as f:
+    with open(nowdir+"\\"+filename.decode('utf-8'), 'wb') as f:
         try:
             while data:
                 f.write(data)
                 data_transferred += len(data)
-                data = serverSocket.recv(1024)
+                data = connectionSocket.recv(1024)
         except Exception as ex:
             print(ex)
             print("파일 받는 과정 오류")
