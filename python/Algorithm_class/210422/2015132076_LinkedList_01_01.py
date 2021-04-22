d03e670c09a074d5f6168d14b81324b6dc47c607
KH-Kim <kh9768@gmail.com>
2021-04-22 13:24:12 +0900

    2021-04-22 김광희 전역변수 제거

--- python/Algorithm_class/210422/2015132076_LinkedList_01_01.py
+++ python/Algorithm_class/210422/2015132076_LinkedList_01_01.py
@@ -40,6 +40,7 @@ class Node() :
         self.data = None
         self.link = None
 
+# 전체 출력
 def printNodes(start) :
     current = start
     if current == None :
@@ -51,106 +52,101 @@ def printNodes(start) :
         print(current.data, end = ' ')
     print()
 
-
-def makeSimpleLinkedList(namePhone) :
-	global  head, current, pre
-	#printNodes(head)
-
-	node = Node()
-	node.data = namePhone
+# 입력모드
+def makeSimpleLinkedList(namePhone, head_node) :
+    node = Node()
+    node.data = namePhone
 	
-	if head == None :			# 첫 번째 노드일 때
-		head = node
-		return
+    if head_node == None :  # 첫 번째 노드일 때
+	    head_node = node
+	    return head_node
 
-	if head.data[0] > namePhone[0] :	# 첫 번째 노드보다 작을 때
-		node.link = head
-		head = node
-		return
+    if head_node.data[0] > namePhone[0] :	# 첫 번째 노드보다 작을 때
+	    node.link = head_node
+	    head_node = node
+	    return head_node
 
 	# 중간 노드로 삽입하는 경우
-	current = head
-	while current.link != None :
-		pre = current
-		current = current.link
-		if current.data[0] > namePhone[0]:
-			pre.link = node
-			node.link = current
-			return
+    current = head_node
+    while current.link != None :
+        pre = current
+        current = current.link
+        if current.data[0] > namePhone[0]:
+            pre.link = node
+            node.link = current
+            return head_node
 
 	# 삽입하는 노드가 가장 큰 경우
-	current.link = node
+    current.link = node
+    return head_node
 
 
 # 수정모드
-def editNode(name):
-    global head
-
-    temp_node = searchNode(name)
+def editNode(name, head):
+    temp_node = searchNode(name, head)
 
     if temp_node == None:
-        return
+        return head
     
     change_number = input(name + "의 변경할 연락처를 입력하세요 : ")
     temp_node.data[1] = change_number
 
     print(temp_node.data, "수정 완료")
+    return head
 
 
 # 삭제모드
-def deleteNode(name):
-    global head, current, pre
-
-    temp_node = searchNode(name)    # searchNode()로 name node 탐색
+def deleteNode(name, head):
+    if searchNode(name, head, True) == None:    # 탐색결과 없으면 return (종료)
+        return head
+    pre_node, current_node = searchNode(name, head, True)    # searchNode()로 name node 탐색
 
     if head.data[0] == name:
-        current = head
+        delete_node = head
         head = head.link
-        del(current)
+        del(delete_node)
         print(name, " 삭제완료")
-        return
+        
 
-    elif temp_node != None:
-        pre.link = current.link
-        del(current)
+    elif current_node != None:
+        pre_node.link = current_node.link
+        del(current_node)
         print(name, " 삭제완료")
-        return
-
     
-# 탐색 모드
-def searchNode(name):
-    global head, current, pre
+    return head
 
-    current = head
+
+# 탐색 모드, 삭제모드 일 때 delete_mode 매개변수 True 전달
+def searchNode(name, head, delete_mode = False):
+    current_node = head
+    pre_node = None
     
-    if current == None:
-        print("연결리스트가 비어 있습니다.")
+    if current_node == None:    # 연결리스트가 비어있음
         return None
             
-    while current != None:    # not null
-        if name == current.data[0]:
+    while current_node != None:    # not null
+        if name == current_node.data[0]:
             break
         
         else :
-            if current.link == None:  # 노드의 link가 비어있으면 다음 노드가 없는것 이므로 해당 연락처가 없는 것
+            if current_node.link == None:  # 노드의 link가 비어있으면 다음 노드가 없는것 이므로 해당 연락처가 없는 것
                 print("해당 연락처를 찾을 수 없습니다.")
                 return None
 
-            pre = current
-            current = current.link
-    
-    return current
+            pre_node = current_node
+            current_node = current_node.link
 
+    if delete_mode: # 삭제모드에서 호출 시 pre_node도 return함
+        return pre_node, current_node
 
-    
+    elif not delete_mode:   # 삭제모드가 아닌 곳에서 호출 시 current_node 만 return
+        return current_node
 
-## 전역 변수 선언 부분 ##
-
-head, current, pre = None, None, None
 
 
 ## 메인 코드 부분 ##
 if __name__ == "__main__" :
+    head = None
 
     while True:
         mode = input("모드를 선택하세요 (0. 종료, 1. 입력, 2. 수정, 3. 삭제, 4. 탐색, 5. 전체 출력) : ")
@@ -167,28 +163,27 @@ if __name__ == "__main__" :
 
             nodedata = [namedata, phonedata]    # 입력받은 데이터를 리스트 형태로 만듬
 
-            makeSimpleLinkedList(nodedata)
+            head = makeSimpleLinkedList(nodedata, head)
             printNodes(head)
         
         # 수정모드
         elif mode == "2":
             inputname = input("수정할 연락처 이름을 입력하세요 : ")
-            editNode(inputname)
+            head = editNode(inputname, head)
             printNodes(head)
 
         # 삭제모드
         elif mode == "3":
             inputname = input("삭제할 연락처 이름을 입력하세요 : ")
-            deleteNode(inputname)
+            head = deleteNode(inputname, head)
             printNodes(head)
 
         # 탐색모드
         elif mode == "4":
             inputname = input("탐색할 연락처 이름을 입력하세요 : ")
-            if searchNode(inputname) != None:
-                print(searchNode(inputname))
-            
-            
+            result_node = searchNode(inputname, head)
+            if result_node != None:
+                print(result_node.data)
             
         # 전체 출력모드
         elif mode == "5":
