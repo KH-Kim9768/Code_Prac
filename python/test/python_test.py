5f0f34a4630c962865947e96771812aa5b7bb8ef
KH-Kim <kh9768@gmail.com>
2021-07-02 22:39:58 +0900

    2021-07-02

--- python/test/python_test.py
+++ python/test/python_test.py
@@ -16,3 +16,9 @@ ii = [0, 1, 2, 3, 4, 5]
 for i in ii:
     i = 0
 print(ii)
+
+i2 = [{'a1' : 'a1 val1', 'a2' : 'a2 val1'}, {'a1' : 'a1 val2', 'a2' : 'a2 val2'}]
+for i in i2:
+    print(i['a1'])
+    print(i['a2'])
+    
