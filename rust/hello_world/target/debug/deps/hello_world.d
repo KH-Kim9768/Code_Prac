626e1bc8219ca84fc24525cae0db03070853fa28
KH-Kim <kh9768@gmail.com>
2021-07-06 00:15:17 +0900

    2021-07-06 김광희

---
+++ rust/hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\KH\Github_Repository\Code_Prac\rust\hello_world\target\debug\deps\hello_world.exe: src\main.rs
+
+C:\Users\KH\Github_Repository\Code_Prac\rust\hello_world\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
---
+++ rust/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\Users\KH\Github_Repository\Code_Prac\rust\hello_world\target\debug\hello_world.exe: C:\Users\KH\Github_Repository\Code_Prac\rust\hello_world\src\main.rs
