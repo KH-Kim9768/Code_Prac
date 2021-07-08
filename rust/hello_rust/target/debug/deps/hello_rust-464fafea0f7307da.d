aa9dc25ef436990ae63f746977065e5bf183f458
KH-Kim <kh9768@gmail.com>
2021-07-09 01:21:58 +0900

    .

---
+++ rust/hello_rust/target/debug/deps/hello_rust-464fafea0f7307da.d
@@ -0,0 +1,5 @@
+c:\Users\KH\Github_Repository\Code_Prac\rust\hello_rust\target\debug\deps\hello_rust-464fafea0f7307da.rmeta: src\main.rs
+
+c:\Users\KH\Github_Repository\Code_Prac\rust\hello_rust\target\debug\deps\hello_rust-464fafea0f7307da.d: src\main.rs
+
+src\main.rs:
---
+++ rust/rust_test/target/debug/deps/rust_test-30a8fa5029fb7fdc.d
@@ -0,0 +1,5 @@
+c:\Users\KH\Github_Repository\Code_Prac\rust\rust_test\target\debug\deps\rust_test-30a8fa5029fb7fdc.rmeta: src\main.rs
+
+c:\Users\KH\Github_Repository\Code_Prac\rust\rust_test\target\debug\deps\rust_test-30a8fa5029fb7fdc.d: src\main.rs
+
+src\main.rs:
