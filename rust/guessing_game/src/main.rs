417772c8061c5a4f5d3cb423f88b3d7d4936f43d
KH-Kim <kh9768@gmail.com>
2021-07-09 01:17:22 +0900

    2021-07-09 김광희 숫자야구게임

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -1,18 +1,39 @@
 use std::io;    // std 표준 라이브러리 -> io 라이브러리 호출
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() { // fn --> 새로운 함수 선언
     println!("Guess the number!");
 
-    println!("Please Input your guess : ");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    loop{
+        println!("Please Input your guess : ");
     
-    let mut guess = String::new();  // let --> 변순 선언, mut --> 가변변수
-    /* rust 에서 변수는 기본적으로 불변이기 때문에 변수 앞에 mut을 이용하여
-     가변변수를 만든다 mutable <-> immutable(default) */
-    
-    guess = 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
-    /* use std::io가 없다면 std::io::stdio 로 작성 */
+        let mut guess = String::new();  // let --> 변순 선언, mut --> 가변변수
+        /* rust 에서 변수는 기본적으로 불변이기 때문에 변수 앞에 mut을 이용하여
+        가변변수를 만든다 mutable <-> immutable(default) */
+        
 
-    println!("You guessed: {}", guess);
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        /* use std::io가 없다면 std::io::stdio 로 작성 */
+
+        // let guess: u32 = guess.trim().parse().expect("Please type a number");
+        let guess: u32 = match guess.trim().parse(){
+            Ok(x) => x,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("TOO SMALL"),
+            Ordering::Greater => println!("TOO BIG"),
+            Ordering::Equal => {
+                println!("YOU WIN");
+                break;
+            }
+        }
+    }
+
+    println!("Bye");
 }
--- rust/guessing_game/target/debug/deps/guessing_game.d
+++ rust/guessing_game/target/debug/deps/guessing_game.d
@@ -1,5 +1,5 @@
-c:\Users\KH\Github_Repository\Code_Prac\rust\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
+C:\Users\KH\Github_Repository\Code_Prac\rust\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
 
-c:\Users\KH\Github_Repository\Code_Prac\rust\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
+C:\Users\KH\Github_Repository\Code_Prac\rust\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
 
 src\main.rs:
