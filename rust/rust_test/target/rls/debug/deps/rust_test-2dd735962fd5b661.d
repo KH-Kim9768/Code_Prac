96af4815732ef80af68b615d00cfb0536bd28ca2
KH-Kim <kh9768@gmail.com>
2021-07-09 01:21:52 +0900

    .

---
+++ rust/rust_test/target/rls/debug/deps/rust_test-2dd735962fd5b661.d
@@ -0,0 +1,5 @@
+c:\Users\KH\Github_Repository\Code_Prac\rust\rust_test\target\rls\debug\deps\rust_test-2dd735962fd5b661.rmeta: src\main.rs
+
+c:\Users\KH\Github_Repository\Code_Prac\rust\rust_test\target\rls\debug\deps\rust_test-2dd735962fd5b661.d: src\main.rs
+
+src\main.rs:
