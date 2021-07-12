f05da453846ba2f89bc549915e0882fe113cb7c2
KH-Kim <kh9768@gmail.com>
2021-07-13 00:46:43 +0900

    2021-07-13 김광희 C# Chapter 2

--- CSharp/Hello/MainApp.cs
+++ CSharp/Hello/MainApp.cs
@@ -7,13 +7,17 @@ namespace Hello
     {
         static void Main(string[] args)
         {
+            /*
             if(args.Length == 0)
             {
                 WriteLine("사용법 : Hello.exe <이름>");
                 return;
             }
-            
+
             WriteLine("Hello, {0}", args[0]);
+            */
+
+            WriteLine("여러분, 안녕하세요?\n반갑습니다!");
         }
     }
 }
