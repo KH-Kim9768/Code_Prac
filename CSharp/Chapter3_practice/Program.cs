e62a6e562ece650891e8c18a1b6fa4a98a076931
KH-Kim <kh9768@gmail.com>
2021-07-13 23:47:48 +0900

    2021-07-13 김광희 C# 챕터3 끝

---
+++ CSharp/Chapter3_practice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Chapter3_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 사각형의 넓이 구하기
+            Console.WriteLine("사각형의 가로 : ");
+            int width = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("사각형의 세로 : ");
+            int height = int.Parse(Console.ReadLine());
+
+            int area = width * height;
+
+            Console.WriteLine("사각형의 넓이 : {0}", area);
+
+
+        }
+    }
+}
---
+++ CSharp/Constant/MainApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Constant
+{
+    class MainApp
+    {
+        enum DialogResult { YES = 10, NO = 20, CANCEL, CONFIRM, OK = 30 }
+
+        static void Main(string[] args)
+        {
+            /*
+            const int MAX_INT = 2147483647;
+            const int MIN_INT = -2147483648;
+
+            Console.Write(MAX_INT);
+            Console.Write(MIN_INT);
+            */
+
+            DialogResult result = DialogResult.YES;
+
+
+            Console.WriteLine((int)DialogResult.YES);
+            Console.WriteLine((int)DialogResult.NO);
+            Console.WriteLine((int)DialogResult.CANCEL);
+            Console.WriteLine((int)DialogResult.CONFIRM);
+            Console.WriteLine((int)DialogResult.OK);
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
