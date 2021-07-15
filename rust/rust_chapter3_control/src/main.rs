70a404832c1a8c043ad3ffd6bcfa146f15dd14b0
KH-Kim <kh9768@gmail.com>
2021-07-15 19:00:46 +0900

    2021-07-15 김광희 rust

--- rust/rust_chapter3_control/src/main.rs
+++ rust/rust_chapter3_control/src/main.rs
@@ -1,8 +1,38 @@
 fn main() {
-    let number = 7;
+    // 조건문
+    /*
+    let condition = true;
+    let number = if condition{
+        7
+    } else {
+        11
+    };
+
     if number < 5 {
-        println!("true");
-    } else{
-        println!("false");
+        println!("number : ~ 5");
+    } else if number > 5 && number <10 {
+        println!("number : 5 ~ 10");
+    } else {
+        println!("numver : 10 ~")
     }
+    */
+
+    // 반복문, 반복
+    let mut number = 5;
+    let a = [10, 20, 30, 40, 50];
+
+    
+    for element in a.iter(){
+        println!("{}~", element);
+    }
+    
+    for number in (1..4).rev(){ // 1 ~ 3 까지
+        println!("{}", number);
+    }
+
+    while number != 0 {
+        println!("{}!",number);
+        number = number - 1 ;
+    }
+    
 }
