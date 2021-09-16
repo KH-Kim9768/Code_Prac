bd19a85da3ec1e791b93b66e1fbc5a24a8257234
KH-Kim9768 <kh9768@gmail.com>
2021-09-16 20:52:52 +0900

    rust

---
+++ rust/rust_chapter13/target/rls/debug/deps/rust_chapter13-8527d83d1132b252.d
@@ -0,0 +1,5 @@
+c:\Users\kh\Github_Repositories\Code_Prac\rust\rust_chapter13\target\rls\debug\deps\rust_chapter13-8527d83d1132b252.rmeta: src\main.rs
+
+c:\Users\kh\Github_Repositories\Code_Prac\rust\rust_chapter13\target\rls\debug\deps\rust_chapter13-8527d83d1132b252.d: src\main.rs
+
+src\main.rs:
---
+++ rust/rust_chapter13/target/rls/debug/deps/rust_chapter13-af6d547d61a4e41a.d
@@ -0,0 +1,5 @@
+c:\Users\kh\Github_Repositories\Code_Prac\rust\rust_chapter13\target\rls\debug\deps\rust_chapter13-af6d547d61a4e41a.rmeta: src\main.rs
+
+c:\Users\kh\Github_Repositories\Code_Prac\rust\rust_chapter13\target\rls\debug\deps\rust_chapter13-af6d547d61a4e41a.d: src\main.rs
+
+src\main.rs:
