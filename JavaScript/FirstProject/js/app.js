4c3e08094df75ce4b56ae7f4403b772b5606e91e
KH-Kim9768 <kh9768@gmail.com>
2021-09-16 20:48:48 +0900

    자바스크립트 연습

--- JavaScript/FirstProject/js/app.js
+++ JavaScript/FirstProject/js/app.js
@@ -1,2 +1,12 @@
 // alert("hi");
+let x = 10;
+let y = 5;
+const myName = "KKh";
+let veryLingVariableName = 0;
+
+console.log(x+y);
+console.log(x*y);
+console.log(x-y);
+console.log(x/y);
+console.log("hello " + myName);
 
