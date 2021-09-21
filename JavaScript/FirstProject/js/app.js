6a516cb3251e21fd163c8d5f978c4a0dc76b2f6f
KH-Kim9768 <kh9768@gmail.com>
2021-09-22 03:10:43 +0900

    JavaScript

--- JavaScript/FirstProject/js/app.js
+++ JavaScript/FirstProject/js/app.js
@@ -10,3 +10,42 @@ console.log(x-y);
 console.log(x/y);
 console.log("hello " + myName);
 
+const asd = true;
+const nullvar = null;
+let undefvar; // undefined
+
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sund = "sund";
+
+const daysOfWeek = [mon, tue, wed, thu, fri, sat];
+
+const nonsense = [1, 2, "hello", true, false, null, undefined, "kkh"];
+
+// Get Item from Array
+console.log(daysOfWeek[2]);
+
+// Add one more day to the array
+daysOfWeek.push(sund);
+console.log(daysOfWeek);
+
+const toBuy = ["potato", "tomato", "pizza"];
+toBuy.push("kimbab");
+
+const player = {
+    name : "kkh",
+    points : 124213,
+    login : false,
+
+};
+
+console.log(player);
+console.log(player.name);
+console.log(player["name"]);
+
+player.lastName = "poatato";
+console.log(player);
