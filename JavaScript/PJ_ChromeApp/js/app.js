2cc257eaca9183715d59f7d6a4f3a5e5e77b160d
KH-Kim9768 <kh9768@gmail.com>
2021-10-08 16:20:47 +0900

    JavaScript Login User practice

--- JavaScript/PJ_ChromeApp/js/app.js
+++ JavaScript/PJ_ChromeApp/js/app.js
@@ -1,15 +1,46 @@
+// CSS CLASS NAME
+const HIDDEN_CLASSNAME = "hidden";
+
+// CONST VAR
+const USERNAME_KEY = "username";
 
 // Login Part
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
 
 /// Login Btn Click function
 function onLoginSubmit(event) {
-    // const username = loginInput.value;
-    // console.log(username);
+
+    // 기본동작을 막는다 preventDefault()
     event.preventDefault();
-    console.log(loginInput.value);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    
+    paintGreetings(username);
     
 }
 
+// Show Greetings
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`; // "Hello " + username
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+// ADD EVENTLISTNER
 loginForm.addEventListener("submit", onLoginSubmit);
+
+
+// Loading Username
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+if (savedUserName == null) {
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+
+} else {
+    // show the greetings
+    paintGreetings(savedUserName);
+}
---
+++ JavaScript/PJ_ChromeApp/js/comments.js
@@ -0,0 +1,19 @@
+/*
+
+이곳은 주석들을 모아 두는 곳입니다
+프로그램에는 영향이 없습니다
+
+*/
+
+// const link = document.querySelector("a");
+
+// function handleLinkClick(event) {
+    
+//     event.preventDefault()
+//     console.log(event);
+//     alert("Link Clicked");
+// }
+
+// link.addEventListener("click", handleLinkClick);
+
+
