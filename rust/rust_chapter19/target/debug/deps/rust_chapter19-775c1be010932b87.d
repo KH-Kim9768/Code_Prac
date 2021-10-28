07152dc286ad5e2decdc7749f2bd1940a3b47570
KH-Kim9768 <kh9768@gmail.com>
2021-10-28 20:56:52 +0900

    rust_chapter19

---
+++ rust/rust_chapter19/target/debug/deps/rust_chapter19-775c1be010932b87.d
@@ -0,0 +1,5 @@
+c:\Users\kh\Github_Repositories\Code_Prac\rust\rust_chapter19\target\debug\deps\rust_chapter19-775c1be010932b87.rmeta: src\main.rs
+
+c:\Users\kh\Github_Repositories\Code_Prac\rust\rust_chapter19\target\debug\deps\rust_chapter19-775c1be010932b87.d: src\main.rs
+
+src\main.rs:
