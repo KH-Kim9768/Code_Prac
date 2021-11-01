7d9aae58884dcacb4cb0a603538c8c4fed2a1765
KH-Kim9768 <kh9768@gmail.com>
2021-11-01 13:37:48 +0900

    rust.

---
+++ rust/rust_chapter19/target/rls/debug/deps/rust_chapter19-8f1cac49927324f5.d
@@ -0,0 +1,5 @@
+c:\Users\kh\Github_Repositories\Code_Prac\rust\rust_chapter19\target\rls\debug\deps\rust_chapter19-8f1cac49927324f5.rmeta: src\main.rs
+
+c:\Users\kh\Github_Repositories\Code_Prac\rust\rust_chapter19\target\rls\debug\deps\rust_chapter19-8f1cac49927324f5.d: src\main.rs
+
+src\main.rs:
